35d1c9355ac4dfa366a2d90bdc70d0ac7d99d675
Ivan <i.sorokin4@g.nsu.ru>
2026-04-21 21:14:30 +0500

    1) добавлены тесты с крайними случаями 2) скорректирована логика работы функций 3) код очищен от лишних функций

--- boyer_mur/main.py
+++ boyer_mur/main.py
@@ -1,29 +1,22 @@
-def max(b, a=1):
+def max(a, b):
     return a if a > b else b
 
-def generate_table(needle: str) -> dict:
-    table = {}
+def generate_char_table(needle: str) -> dict:
+    table = dict()
     index = 0
-    for i in needle:
-        table[i] = max(len(needle) - index - 1)
+    for char in needle:
+        table[char] = max(1, len(needle) - index - 1)
         index += 1
     return table
 
-def key_is_exist(table: dict, key: str) -> bool:
-    for i in table:
-        if i == key:
-            return True
-    return False
-
-def is_same_elems(haystack_elem: str, needle_elem: str) -> bool:
-    return haystack_elem == needle_elem
-
 def index_after_shift(haystack, needle, needle_shift_table, i, j) -> int:
     for haystack_elem, needle_elem in zip(range(i, -1, -1), range(j, -1, -1), strict=False):
-        if is_same_elems(haystack[haystack_elem], needle[needle_elem]):
+        if haystack[haystack_elem] == needle[needle_elem]:
             continue
-        if key_is_exist(needle_shift_table, key := haystack[haystack_elem]):
-            return haystack_elem + needle_shift_table[key]
+        key = haystack[haystack_elem]
+        if key in needle_shift_table.keys():
+            new_index = haystack_elem + needle_shift_table[key]
+            return new_index if new_index > i else i + 1            
         return haystack_elem + len(needle)
     return 0
 
@@ -34,11 +27,11 @@ def my_strstr(haystack: str, needle: str) -> int:
     i = j
     while i < len(haystack):
         saved_i = i
-        i = index_after_shift(haystack, needle, generate_table(needle), i, j)
+        i = index_after_shift(haystack, needle, generate_char_table(needle), i, j)
         if i == 0:
-            return saved_i - len(needle) + 1
-    else:
-        return -1
+            return saved_i - len(needle) + 1 
+    return -1
+    
 
 
     
--- boyer_mur/test_boyer_mur.py
+++ boyer_mur/test_boyer_mur.py
@@ -1,5 +1,5 @@
 import pytest
-from main import generate_table, max, key_is_exist, is_same_elems, index_after_shift, my_strstr
+from main import generate_char_table, max, index_after_shift, my_strstr
 
 @pytest.mark.parametrize("second_number, expected", [
     (1, 1),
@@ -8,7 +8,7 @@ from main import generate_table, max, key_is_exist, is_same_elems, index_after_s
     (-1, 1),
 ])
 def test_max(second_number, expected):
-    assert max(second_number) == expected
+    assert max(1, second_number) == expected
 
 @pytest.mark.parametrize("string, expected", [
     ("123", {"1":2, "2":1, "3":1}),
@@ -17,39 +17,24 @@ def test_max(second_number, expected):
     ("1488HH", {"1":5, "4":4, "8":2, "H":1}),
     ("", {}),
 ])
-def test_generate_table(string, expected):
-    assert generate_table(string) == expected
+def test_generate_char_table(string, expected):
+    assert generate_char_table(string) == expected
 
-@pytest.mark.parametrize("dict, key, expected", [
-    ({"1":5, "4":4, "8":2, "H":1}, "H", True),
-    ({"1":5, "4":4, "8":2, "H":1}, "2", False),
-    ({}, "H", False),
-    ({"H":3}, "H", True),
-    ({"H":3}, "T", False)
+@pytest.mark.parametrize("haystack, needle, last_index_needle, last_index_haystack, needle_shift_table, expected", [
+    ("tree", "ee", 1, 1, generate_char_table("ee"), 3),
+    ("tree", "tr", 1, 1, generate_char_table("tr"), 0),
+    ("AbbA", "bb", 1, 1, generate_char_table("bb"), 2),
+    ("AAAbbA", "bb", 1, 1, generate_char_table("bb"), 3),
+    ("AAAbbA", "bb", 1, 3, generate_char_table("bb"), 4),
+    ("AbbErtDF", "GreE", 3, 3, generate_char_table("GreE"), 6),
+    ("AbbErtDF", "Gre", 2, 2, generate_char_table("Gre"), 5),
+    ("xaca", "abca", 3, 3, generate_char_table("abca"), 4),
 ])
-def test_key_check(dict, key, expected):
-    assert key_is_exist(dict, key) == expected
-
-@pytest.mark.parametrize("haystack_elem, needle_elem, expected", [
-    ("", "", True),
-    ("1", "1", True),
-    ("1", "0", False),
-    ("0", "1", False),
-    ("A", "a", False)
-])
-def test_is_same(haystack_elem, needle_elem, expected):
-    assert is_same_elems(haystack_elem, needle_elem) == expected
-
-@pytest.mark.parametrize("haystack, needle, last_index_needle, needle_shift_table, expected", [
-    ("tree", "ee", 1, generate_table("ee"), 3),
-    ("tree", "tr", 1, generate_table("tr"), 0),
-    ("AbbA", "bb", 1, generate_table("bb"), 2),
-    ("AbbErtDF", "GreE", 2, generate_table("GreE"), 6),
-])
-def test_index_after_shift(haystack, needle, last_index_needle, needle_shift_table, expected):
+def test_index_after_shift(haystack, needle, last_index_needle, last_index_haystack, needle_shift_table, expected):
     j = last_index_needle
-    i = j
+    i = last_index_haystack
     assert index_after_shift(haystack, needle, needle_shift_table, i, j) == expected
+
 
 @pytest.mark.parametrize("haystack, needle, expected", [
     ("babokat123", "", -1),
@@ -62,6 +47,10 @@ def test_index_after_shift(haystack, needle, last_index_needle, needle_shift_tab
     ("babo123kat123", "123", 4),
     ("babokat123", "RE", -1),
     ("babokat123", "babokat123", 0),
+    ("xaca", "abca", -1),
+    ("xaca......", "abca", -1),
+    ("aaabcaxaca......", "abca", 2),
+    ("11231xaca......", "abca", -1),
 ])
 def test_my_strstr(haystack, needle, expected):
     assert my_strstr(haystack, needle) == expected
